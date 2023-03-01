a55c1f01c31de503246e5d754fa9c49a3ce234dd
Ars <asd>
2023-03-02 01:09:26 +0300

    Created controllers

--- src/main/java/com/cashew/budgetservice/controllers/ExpensesController.java
+++ src/main/java/com/cashew/budgetservice/controllers/ExpensesController.java
@@ -1,13 +1,43 @@
 package com.cashew.budgetservice.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/v1/expenses")
 public class ExpensesController {
+//    @Autowired
+//    private ExpensesDAO dao;
 
-    // list of expenses for day
-    // list of expenses for week
-    // list of expenses for month
-    // list of expenses for year(expenses for each month)
-    // list of expenses for custom period
+    @GetMapping(path = "/{id}/day")
+    public ResponseEntity<?> getExpensesPerDay(@PathVariable(value = "id") Long id){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping(path = "/{id}/week")
+    public ResponseEntity<?> getExpensesPerWeek(@PathVariable(value = "id") Long id){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping(path = "/{id}/month")
+    public ResponseEntity<?> getExpensesPerMonth(@PathVariable(value = "id") Long id){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping(path = "/{id}/year")
+    public ResponseEntity<?> getExpensesPerYear(@PathVariable(value = "id") Long id){
+        // list of expenses for year(expenses for each month)
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping(path = "/{id}/period")
+    public ResponseEntity<?> getExpensesPerCustomPeriod(
+            @PathVariable(value = "id") Long id,
+            @RequestParam String from,
+            @RequestParam String to){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
--- src/main/java/com/cashew/budgetservice/controllers/FriendsController.java
+++ src/main/java/com/cashew/budgetservice/controllers/FriendsController.java
@@ -1,14 +1,75 @@
 package com.cashew.budgetservice.controllers;
 
+import com.cashew.budgetservice.DAO.Interfaces.FriendsDAO;
+import com.cashew.budgetservice.DTO.StatusDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/v1/friend")
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api/v1/friends")
 public class FriendsController {
+    @Autowired
+    private FriendsDAO dao;
 
-    // list of friends
+    @GetMapping(path = "/{username}")
+    public ResponseEntity<?> getFriends(@PathVariable(value = "username") String username){
+        try {
+            return new ResponseEntity(dao.getFriends(username), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new StatusDTO(404,"User not found").toJson(), HttpStatus.NOT_FOUND);
+        }
+    }
 
-    // add user to friend
+    @GetMapping(path = "/{username}/requests")
+    public ResponseEntity<?> getFriendRequests(@PathVariable(value = "username") String username){
+        try {
+            return new ResponseEntity(dao.getFriendRequests(username), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new StatusDTO(404,"User not found").toJson(), HttpStatus.NOT_FOUND);
+        }
+    }
 
-    // delete user from friend
+    @PostMapping("/request/send")
+    public ResponseEntity<?> sendRequest(@RequestParam String sender, @RequestParam String recipient){
+        try {
+            dao.sendRequest(sender, recipient);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new StatusDTO(404,e.getMessage()).toJson(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/request/accept")
+    public ResponseEntity<?> acceptRequest(@RequestParam String sender, @RequestParam String recipient){
+        try {
+            dao.acceptRequest(sender, recipient);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new StatusDTO(404,e.getMessage()).toJson(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/request/decline")
+    public ResponseEntity<?> declineRequest(@RequestParam String sender, @RequestParam String recipient){
+        try {
+            dao.declineRequest(sender, recipient);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new StatusDTO(404,e.getMessage()).toJson(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/remove/")
+    public ResponseEntity<?> removeFromFriends(@RequestParam String deleter, @RequestParam String deleted){
+        try {
+            dao.deleteFromFriends(deleter, deleted);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new StatusDTO(404,e.getMessage()).toJson(), HttpStatus.NOT_FOUND);
+        }
+    }
 }
