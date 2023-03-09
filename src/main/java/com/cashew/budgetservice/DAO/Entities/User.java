4783a9a8e71c28974683e6310f4ab2a2e9bdb550
Ars <asd>
2023-03-09 16:31:08 +0300

    Prettify

--- src/main/java/com/cashew/budgetservice/DAO/Entities/User.java
+++ src/main/java/com/cashew/budgetservice/DAO/Entities/User.java
@@ -3,32 +3,27 @@ package com.cashew.budgetservice.DAO.Entities;
 import com.cashew.budgetservice.DAO.CustomSerializers.UserSerializer;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @JsonSerialize(using = UserSerializer.class)
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    @Getter @Setter
     private Long id;
 
-    @Getter @Setter
     private String username;
 
-    @Getter @Setter
     private String email;
 
     @CreatedDate
-    @Getter @Setter
     private LocalDateTime date;
 
     @OneToOne(cascade=CascadeType.PERSIST)
-    @Getter @Setter
     private UserDetails userDetails;
 }
 
