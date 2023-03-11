7445c539918206a8be19c3fd67b8bb763389eec9
Ars <asd>
2023-03-11 18:09:43 +0300

    Made UsersService, UserDTO. Refactored UserController, User, UserDetails

---
+++ src/main/java/com/cashew/budgetservice/DTO/UsersDTO.java
@@ -0,0 +1,61 @@
+package com.cashew.budgetservice.DTO;
+
+import lombok.Data;
+import lombok.Value;
+
+public enum UsersDTO {;
+    private interface Id { Long getId(); }
+    private interface Username { String getUsername(); }
+    private interface Email { String getEmail(); }
+
+    public enum Request{;
+        @Data
+        public static class Create implements Username, Email {
+            String username;
+            String email;
+        }
+
+        @Data
+        public static class UpdateUser implements Id, Username, Email{
+            Long id;
+            String username;
+            String email;
+        }
+
+        @Data
+        public static class GetByEmail implements Email{
+            String email;
+        }
+
+        @Data
+        public static class GetByUsername implements Username{
+            String username;
+        }
+
+        @Data
+        public static class Delete implements Id{
+            Long id;
+        }
+    }
+
+    public enum Response{;
+        @Value
+        public static class Created extends DTO implements Id{
+            Long id;
+        }
+
+        @Value
+        public static class Updated extends DTO implements Id, Username, Email{
+            Long id;
+            String username;
+            String email;
+        }
+
+        @Value
+        public static class Found extends DTO implements Id, Username, Email{
+            Long id;
+            String username;
+            String email;
+        }
+    }
+}
