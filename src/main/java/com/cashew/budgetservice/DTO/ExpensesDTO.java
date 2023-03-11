780389a72ec696bede431a98fc6d50443367459c
Ars <asd>
2023-03-11 14:53:33 +0300

    Prettify expenses DTO,DAO,Controller

--- src/main/java/com/cashew/budgetservice/DTO/ExpensesDTO.java
+++ src/main/java/com/cashew/budgetservice/DTO/ExpensesDTO.java
@@ -1,44 +1,41 @@
 package com.cashew.budgetservice.DTO;
 
+import com.cashew.budgetservice.DAO.Entities.Receipt;
 import com.cashew.budgetservice.DAO.Entities.UserCheck;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public enum ExpensesDTO {;
     private interface Username { String getUsername(); }
-    private interface Expenses { List<UserCheck> getExpenses();}
+    private interface Expenses { List<Receipt> getExpenses();}
 
     public enum Request{;
 
         @Data
-        @NoArgsConstructor
         public static class perLastDay implements Username {
             String username;
         }
 
         @Data
-        @NoArgsConstructor
         public static class perLastWeek implements Username {
             String username;
         }
 
         @Data
-        @NoArgsConstructor
         public static class perLastMonth implements Username {
             String username;
         }
 
         @Data
-        @NoArgsConstructor
         public static class perLastYear implements Username {
             String username;
         }
 
         @Data
-        @NoArgsConstructor
         public static class perCustomPeriod implements Username {
             String username;
             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
@@ -49,10 +46,17 @@ public enum ExpensesDTO {;
     }
 
     public enum Response{;
-        @EqualsAndHashCode(callSuper = true)
-        @Value
-        public static class Expenses extends DTO implements ExpensesDTO.Expenses{
-            List<UserCheck> expenses;
+        @Data
+        public static class RequestedChecks extends DTO implements Expenses{
+            List<Receipt> expenses;
+
+            public RequestedChecks setExpenses(Iterable<UserCheck> checks) {
+                expenses = new ArrayList<>();
+                checks.forEach((check) -> {
+                    expenses.add(check.getReceipt());
+                });
+                return this;
+            }
         }
     }
 }
--- src/main/java/com/cashew/budgetservice/services/ExpensesService.java
+++ src/main/java/com/cashew/budgetservice/services/ExpensesService.java
@@ -1,6 +1,7 @@
 package com.cashew.budgetservice.services;
 
 import com.cashew.budgetservice.DAO.Interfaces.ExpensesDAO;
+import com.cashew.budgetservice.DTO.DTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,24 +18,24 @@ public class ExpensesService {
         this.expensesDAO = expensesDAO;
     }
 
-    public ResponseEntity<String> getExpensesPerLastDay(String username){
-        return new ResponseEntity<>(expensesDAO.getExpensesPerLastDay(username).toJson(), HttpStatus.OK);
+    public ResponseEntity<DTO> getExpensesPerLastDay(String username){
+        return new ResponseEntity<>(expensesDAO.getExpensesPerLastDay(username), HttpStatus.OK);
     }
 
-    public ResponseEntity<String> getExpensesPerLastWeek(String username){
-        return new ResponseEntity<>(expensesDAO.getExpensesPerLastWeek(username).toJson(), HttpStatus.OK);
+    public ResponseEntity<DTO> getExpensesPerLastWeek(String username){
+        return new ResponseEntity<>(expensesDAO.getExpensesPerLastWeek(username), HttpStatus.OK);
     }
 
-    public ResponseEntity<String> getExpensesPerLastMonth(String username){
-        return new ResponseEntity<>(expensesDAO.getExpensesPerLastMonth(username).toJson(), HttpStatus.OK);
+    public ResponseEntity<DTO> getExpensesPerLastMonth(String username){
+        return new ResponseEntity<>(expensesDAO.getExpensesPerLastMonth(username), HttpStatus.OK);
     }
 
-    public ResponseEntity<String> getExpensesPerLastYear(String username){
-        return new ResponseEntity<>(expensesDAO.getExpensesPerLastYear(username).toJson(), HttpStatus.OK);
+    public ResponseEntity<DTO> getExpensesPerLastYear(String username){
+        return new ResponseEntity<>(expensesDAO.getExpensesPerLastYear(username), HttpStatus.OK);
     }
 
-    public ResponseEntity<String> getExpensesPerCustomPeriod(String username, LocalDateTime from, LocalDateTime to){
+    public ResponseEntity<DTO> getExpensesPerCustomPeriod(String username, LocalDateTime from, LocalDateTime to){
 
-        return new ResponseEntity<>(expensesDAO.getExpensesPerCustomPeriod(username, from, to).toJson(), HttpStatus.OK);
+        return new ResponseEntity<>(expensesDAO.getExpensesPerCustomPeriod(username, from, to), HttpStatus.OK);
     }
 }
