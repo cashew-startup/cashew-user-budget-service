e9ee6d68e8ecb5c08d0f538b37bb20d45af8fad8
Ars <asd>
2023-03-25 13:24:31 +0100

    Integration tests for : PartiesService, FriendsService, UsersService

---
+++ src/test/java/com/cashew/budgetservice/Integration/PartyControllerTest.java
@@ -0,0 +1,314 @@
+package com.cashew.budgetservice.Integration;
+
+import com.cashew.budgetservice.services.PartiesService;
+import com.cashew.budgetservice.services.UsersService;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PartyControllerTest {
+    private PartiesService partiesService;
+    private UsersService usersService;
+    private MockMvc mockMvc;
+
+    private Long ownerUserId;
+    private Long guestUserId1;
+    private Long guestUserId2;
+    private Long testPartyId;
+    private String testPartyName = "TestParty";
+    private String ownerUsername = "owneruser";
+    private String guest1Username = "guestuser1";
+    private String guest2Username = "guestuser2";
+
+    @Autowired
+    public PartyControllerTest(PartiesService partiesService, UsersService usersService, MockMvc mockMvc) {
+        this.partiesService = partiesService;
+        this.usersService = usersService;
+        this.mockMvc = mockMvc;
+    }
+
+    @BeforeAll
+    public void setUp() {
+        ownerUserId = usersService.createUser(ownerUsername, "example@g.com").getBody().getId();
+        guestUserId1 = usersService.createUser(guest1Username, "example@y.com").getBody().getId();
+        guestUserId2 = usersService.createUser(guest2Username, "example@t.com").getBody().getId();
+    }
+
+    @AfterAll
+    void closeUp() {
+        usersService.deleteUserById(ownerUserId);
+        usersService.deleteUserById(guestUserId1);
+        usersService.deleteUserById(guestUserId2);
+    }
+
+    @Test
+    @Order(1)
+    public void testCannotCreatePartyDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(post("http://localhost:8080/api/v1/parties")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "name": "%s",
+                                    "ownerId": 0
+                                }""".formatted(testPartyName)))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No user with such id"));
+    }
+
+    @Test
+    @Order(2)
+    public void testCreatePartySuccessfully() throws Exception {
+        /*Create party and remember its id*/
+        ResultActions response = this.mockMvc
+                .perform(post("http://localhost:8080/api/v1/parties")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "name": "%s",
+                                    "ownerId": %s
+                                }""".formatted(testPartyName, ownerUserId)));
+        response.andExpectAll(
+                status().isCreated(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("partyId").exists());
+        testPartyId = Long.valueOf(JsonPath.parse(response.andReturn().getResponse().getContentAsString()).read("partyId").toString());
+    }
+
+    @Test
+    @Order(3)
+    public void testCannotAddUserToPartyDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(patch("http://localhost:8080/api/v1/parties/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": 0,
+                                    "guestId": 0
+                                }"""))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No party with such id"));
+    }
+
+    @Test
+    @Order(4)
+    public void testAddUserToPartySuccessfully() throws Exception {
+        this.mockMvc
+                .perform(patch("http://localhost:8080/api/v1/parties/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": %s,
+                                    "username": "%s"
+                                }""".formatted(testPartyId, guest1Username)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("success").value(true));
+        this.mockMvc
+                .perform(patch("http://localhost:8080/api/v1/parties/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": %s,
+                                    "username": "%s"
+                                }""".formatted(testPartyId, guest2Username)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("success").value(true));
+    }
+
+    @Test
+    @Order(5)
+    public void testCannotRemoveUserFromPartyDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(delete("http://localhost:8080/api/v1/parties/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": 0,
+                                    "guestId": 0
+                                }"""))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No party with such id"));
+    }
+
+    @Test
+    @Order(6)
+    public void testRemoveUserFromPartySuccessfully() throws Exception {
+        this.mockMvc
+                .perform(delete("http://localhost:8080/api/v1/parties/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": %s,
+                                    "username": "%s"
+                                }""".formatted(testPartyId, guest1Username)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("success").value(true));
+    }
+
+    @Test
+    @Order(7)
+    public void testCannotGetFullPartyInfoDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(get("http://localhost:8080/api/v1/parties/0/info"))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No party with such id"));
+    }
+
+    @Test
+    @Order(8)
+    public void testGetFullPartyInfoSuccessfully() throws Exception {
+        this.mockMvc
+                .perform(get("http://localhost:8080/api/v1/parties/%s/info".formatted(testPartyId)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("party.id").value(testPartyId),
+                        jsonPath("party.['owner id']").value(ownerUserId),
+                        jsonPath("party.users").value(hasItems(ownerUsername,guest1Username,guest2Username)));
+    }
+
+    @Test
+    @Order(9)
+    public void testCannotGetPartiesOfUserDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(get("http://localhost:8080/api/v1/parties/of")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": ""
+                                }"""))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No user with such username"));
+    }
+
+    @Test
+    @Order(10)
+    public void testGetPartiesOfUserSuccessfully() throws Exception {
+        this.mockMvc
+                .perform(get("http://localhost:8080/api/v1/parties/of")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": "%s"
+                                }""".formatted(ownerUsername)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("parties[0].id").value(testPartyId));
+        this.mockMvc
+                .perform(get("http://localhost:8080/api/v1/parties/of")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": "%s"
+                                }""".formatted(guest1Username)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("parties[0].id").value(testPartyId));
+    }
+
+    @Test
+    @Order(11)
+    public void testCannotChangePartyNameDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(patch("http://localhost:8080/api/v1/parties")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": 0,
+                                    "name": "new name"
+                                }"""))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No party with such id"));
+    }
+
+    @Test
+    @Order(12)
+    public void testChangePartyNameSuccessfully() throws Exception {
+        this.mockMvc
+                .perform(patch("http://localhost:8080/api/v1/parties")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "partyId": %s,
+                                    "name": "new name"
+                                }""".formatted(testPartyId)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("success").value(true));
+    }
+
+
+    @Test
+    @Order(13)
+    public void testCannotDeletePartyDueToInvalidInput() throws Exception {
+        this.mockMvc
+                .perform(delete("http://localhost:8080/api/v1/parties/0"))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No party with such id"));
+    }
+
+    @Test
+    @Order(14)
+    public void testDeletePartySuccessfully() throws Exception {
+        this.mockMvc
+                .perform(delete("http://localhost:8080/api/v1/parties/%s" .formatted(testPartyId)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("success").value(true));
+        this.mockMvc
+                .perform(get("http://localhost:8080/api/v1/parties/%s" .formatted(testPartyId)))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("code").value(404),
+                        jsonPath("description").value("No party with such id"));
+    }
+}
